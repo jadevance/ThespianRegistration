c6bd51f5f66ee379865b9dfc304d8f3f6d97e8b2
Jillian Boshart <Jilliliz@gmail.com>
2017-08-31 14:14:57 -0700

    Reset.

---
+++ thesReg/models/users.js
@@ -0,0 +1,24 @@
+var app = require("../app");
+var db = app.get("db");
+
+var Users = function() {};
+
+Users.findOrMakeUser = function(userSessionInfo, callback) {
+	db.users.findOne({google_id: userSessionInfo.id}, function(error, user) {
+		if (error) {
+			callback(error, undefined)
+		} else if (!user) {
+			db.users.save({google_id: userSessionInfo.id}, function(error, newUser) {
+				if (error || !newUser) {
+					callback(error, undefined)
+				} else {
+					callback(null, newUser)
+				}
+			})
+		} else {
+			callback(null, user)
+		}
+	})
+};
+
+module.exports = Users;
