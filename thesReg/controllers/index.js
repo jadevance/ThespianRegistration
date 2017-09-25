7c25e5bd03b35b57bbeeb09631a10ce1132b5af9
Jillian Boshart <Jilliliz@gmail.com>
2017-09-25 13:27:47 -0700

    Removed superflous title param.

--- thesReg/controllers/index.js
+++ thesReg/controllers/index.js
@@ -3,13 +3,11 @@ var IndexController = {
   getIndex: function(request, response) {
     if(request.user) {
       response.render('index', {
-        title: 'Totes logged in',
         user: request.user,
         loggedIn: true
       })
     } else {
       response.render('index', {
-        title: 'Not logged in',
         loggedIn: false
       })
     }
