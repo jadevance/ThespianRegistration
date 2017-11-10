c7b8cc093ab622e266f678fa42048cd70cb95999
Jillian Boshart <Jilliliz@gmail.com>
2017-11-09 19:06:02 -0800

    More registration stuff. Who knows if it works? Not me.

--- thesReg/models/registrations.js
+++ thesReg/models/registrations.js
@@ -1,5 +1,7 @@
 var app = require('../app.js');
 var db = app.get('db');
+var Promise = require('bluebird');
+var common = require('../controllers/commonFunctions.js');
 
 var Registrations = function() {};
 
@@ -10,7 +12,31 @@ Registrations.getUserRegistrations = function(userId, callback) {
     } else if (!registrations) {
       callback(null, null)
     } else {
-      callback(null, registrations)
+      var registrationPromises = [];
+      for (var i=0; i<registrations.length; i++) {
+        (function i, registrations) {
+          var promise = new Promise(function(resolve, reject) {
+            db.conferences.find({id: registrations[i].conference_id}, function(error, conference) {
+              if (error) {
+                reject(error)
+              } else {
+                registrations[i].ableToRegister = conference.registration_status;
+                resolve()
+              }
+            })
+          })
+          registrationPromises.push(promise)
+        }(i, registrations)
+      }
+
+      Promise.all(registrationPromises).then(
+        function() {
+          callback(null, registrations)
+        },
+        function(error) {
+          callback(error, undefined)
+        }
+      )
     }
   })
 }
