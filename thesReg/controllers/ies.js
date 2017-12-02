2c08cf16cdc97d9513c72e336903536adbfb38cd
Jillian Boshart <Jilliliz@gmail.com>
2017-12-02 10:28:57 -0800

    Scaffolding for show/add/edit student ies.

--- thesReg/controllers/ies.js
+++ thesReg/controllers/ies.js
@@ -2,6 +2,70 @@ var iesModel = require('../models/ies.js');
 
 var iesController = {
 
+  getStudentIes: function(request, response) {
+    iesModel.getStudentIes(request.params.registrationId, request.params.studentId, function(error, ies) {
+      if (error) {
+        var err = new Error;
+        err.status = 500;
+        err.error = "Error getting ies";
+        response.json(err)
+      } else {
+
+      }
+    })
+  },
+
+  // SOLO:
+  getNewSolo: function(request, response) {
+
+  },
+
+  createNewSolo: function(request, response) {
+
+  },
+
+  editSolo: function(request, response) {
+
+  },
+
+  updateSolo: function(request, response) {
+
+  },
+
+  // DUO:
+  getNewDuo: function(request, response) {
+
+  },
+
+  createNewDuo: function(request, response) {
+
+  },
+
+  editDuo: function(request, response) {
+
+  },
+
+  updateDuo: function(request, response) {
+
+  },
+
+  // TECH:
+  getNewTech: function(request, response) {
+
+  },
+
+  createNewTech: function(request, response) {
+
+  },
+
+  editTech: function(request, response) {
+
+  },
+
+  updateTech: function(request, response) {
+
+  },
+
 };
 
 module.exports = iesController;
--- thesReg/routes/registrations.js
+++ thesReg/routes/registrations.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var registrationsController = require('../controllers/registrations.js');
+var iesController = require('../controllers/ies.js');
 
+// New Reg
 router.post('/conferences/:conferenceId/registration/new', registrationsController.createNewRegistration);
 
+// Edit Reg
 router.get('/conferences/:conferenceId/registration/:registrationId/edit', registrationsController.editRegistration);
 
+// Add/Remove Students
 router.get('/conferences/:conferenceId/registration/:registrationId/editStudents', registrationsController.addOrRemoveStudents);
 router.post('/conferences/:conferenceId/registration/:registrationId/editStudents', registrationsController.saveAddOrRemoveStudents);
+
+// Show IES:
+router.get('/conferences/:conferenceId/registration/:registrationId/ies/:studentId', iesController.getStudentIes);
+
+// Solo IES:
+router.get('/conferences/:conferenceId/registration/:registrationId/solo/:studentId/new', iesController.getNewSolo);
+router.post('/conferences/:conferenceId/registration/:registrationId/solo/:studentId/new', iesController.createNewSolo);
+router.get('/conferences/:conferenceId/registration/:registrationId/solo/:studentId/edit/:iesId', iesController.editSolo);
+router.post('/conferences/:conferenceId/registration/:registrationId/solo/:studentId/edit/:iesId', iesController.updateSolo);
+
+// Duo IES:
+router.get('/conferences/:conferenceId/registration/:registrationId/duo/:studentId/new', iesController.getNewDuo);
+router.post('/conferences/:conferenceId/registration/:registrationId/duo/:studentId/new', iesController.createNewDuo);
+router.get('/conferences/:conferenceId/registration/:registrationId/duo/:studentId/edit/:iesId', iesController.editDuo);
+router.post('/conferences/:conferenceId/registration/:registrationId/duo/:studentId/edit/:iesId', iesController.updateDuo);
+
+// Technical IES:
+router.get('/conferences/:conferenceId/registration/:registrationId/tech/:studentId/new', iesController.getNewTech);
+router.post('/conferences/:conferenceId/registration/:registrationId/tech/:studentId/new', iesController.createNewTech);
+router.get('/conferences/:conferenceId/registration/:registrationId/tech/:studentId/edit/:iesId', iesController.editTech);
+router.post('/conferences/:conferenceId/registration/:registrationId/tech/:studentId/edit/:iesId', iesController.updateTech);
+
+
+
 
 module.exports = router;
